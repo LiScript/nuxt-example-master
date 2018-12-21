7815403e2ee1c7a48bcd80b7684fb81149070387
Polilfz <181991667@qq.com>
2018-12-21 10:00:24 +0800

    Revert "first"
    This reverts commit cf7b0c83508fee461619937dd1bb6206a06b072f.

--- plugins/element-ui.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Element from 'element-ui/lib/element-ui.common'
-import locale from 'element-ui/lib/locale/lang/en'
-import './element-variables.scss'
-
-export default () => {
-  Vue.use(Element, { locale })
-}
--- plugins/vue-lazyload.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueLazyLoad from 'vue-lazyload'
-
-Vue.use(VueLazyLoad, {
-  preLoad: 1.3,
-  error: './assets/img/error.jpg',
-  loading: './assets/img/loading.jpg',
-  attempt: 3
-})
